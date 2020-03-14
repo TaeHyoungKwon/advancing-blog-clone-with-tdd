402eb70e65598308be3f8887f8368d014c5c404b
TaeHyoungKwon <kth5604@gmail.com>
2020-03-14 19:52:56 +0900

    delete view with confirmation page

--- src/comments/views.py
+++ src/comments/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+
 from trydjango19.helpers import get_or_create_new_comment
 
 from django.contrib.contenttypes.models import ContentType
@@ -10,17 +12,21 @@ from .models import Comment
 
 def comment_thread(request, id):
     obj = get_object_or_404(Comment, id=id)
-    initial_data = {
-            "content_type": obj.content_type,
-            "object_id": obj.object_id
-    }
+    initial_data = {"content_type": obj.content_type, "object_id": obj.object_id}
     form = CommentForm(request.POST or None, initial=initial_data)
     if form.is_valid():
         new_comment = get_or_create_new_comment(form, request)
         return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
 
-    context = {
-        "comment": obj,
-        "form": form,
-    }
+    context = {"comment": obj, "form": form}
     return render(request, "comment_thread.html", context)
+
+
+def comment_delete(request, id):
+    obj = get_object_or_404(Comment, id=id)
+    if request.method == "POST":
+        parent_obj_url = obj.content_object.get_absolute_url()
+        obj.delete()
+        messages.success(request, "This has been deleted.")
+        return HttpResponseRedirect(parent_obj_url)
+    return render(request, "confirm_delete.html", {"object": obj})

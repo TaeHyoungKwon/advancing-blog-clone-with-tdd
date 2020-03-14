274e4cd55b4e51d7cf0e1e2816aae70c52341fb7
TaeHyoungKwon <kth5604@gmail.com>
2020-03-14 16:41:02 +0900

    django crispy forms

--- src/trydjango19/settings.py
+++ src/trydjango19/settings.py
@@ -41,10 +41,13 @@ INSTALLED_APPS = [
     # Third party
     'pagedown',
     'markdown_deux',
+    'crispy_forms',
 
     # Local apps
     'posts',
 ]
+
+CRISPY_TEMPLATE_PACK = 'boostrap3'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
